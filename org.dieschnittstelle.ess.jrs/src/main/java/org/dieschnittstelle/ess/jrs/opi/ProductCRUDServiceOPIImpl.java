bbc0dcbefceab1bb5b1687a57b6eb58c4cf0f660
Urs Kahmann <urka3288@bht-berlin.de>
2023-05-16 17:16:33 +0200

    Aufgabe OPI 1

--- org.dieschnittstelle.ess.jrs/src/main/java/org/dieschnittstelle/ess/jrs/opi/ProductCRUDServiceOPIImpl.java
+++ org.dieschnittstelle.ess.jrs/src/main/java/org/dieschnittstelle/ess/jrs/opi/ProductCRUDServiceOPIImpl.java
@@ -3,11 +3,14 @@ package org.dieschnittstelle.ess.jrs.opi;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.dieschnittstelle.ess.entities.crm.StationaryTouchpoint;
 import org.dieschnittstelle.ess.entities.erp.Campaign;
 import org.dieschnittstelle.ess.entities.erp.IndividualisedProductItem;
 import org.dieschnittstelle.ess.jrs.IProductCRUDService;
 import org.dieschnittstelle.ess.jrs.ProductCRUDServiceImpl;
+import org.dieschnittstelle.ess.jrs.TouchpointCRUDServiceImpl;
+import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
 import org.eclipse.microprofile.openapi.annotations.media.Content;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
@@ -25,6 +28,9 @@ import jakarta.ws.rs.core.MediaType;
  *  Zugegriffen wird auf diese Implementierung aus dem Testcase TestProductRESTServiceWithOpenAPI
  */
 // TODO: verwenden Sie die URI opi/products
+@Path("opi/products")
+@Consumes({MediaType.APPLICATION_JSON})
+@Produces({MediaType.APPLICATION_JSON})
 public class ProductCRUDServiceOPIImpl {
 
 	private IProductCRUDService service;
@@ -37,7 +43,12 @@ public class ProductCRUDServiceOPIImpl {
 	 * TODO: implementieren Sie einen weiteren Konstruktor, der es Ihnen erlaubt, das service Attribut
 	 *  so zu instantiieren, dass es zur Laufzeit erfolgreich verwendet werden kann
 	 */
+	public ProductCRUDServiceOPIImpl(@Context ServletContext servletContext) {
+		this.service = new ProductCRUDServiceImpl(servletContext);
+	}
 
+	@POST
+	@Operation(operationId = "createProduct")
 	public IndividualisedProductItem createProduct(
 			IndividualisedProductItem prod) {
 		return (IndividualisedProductItem)this.service.createProduct(prod);
@@ -45,12 +56,16 @@ public class ProductCRUDServiceOPIImpl {
 
 	// TODO: ueberlegen Sie, wie Sie createCampaign() von createProduct() unterscheidbar machen koennen - wenn
 	//  Sie JRS3 umgesetzt haben, koennen Sie die derzeit noch auskommentierte Codezeile aktivieren
+	@POST
+	@Path("/campaigns")
+	@Operation(operationId = "createCampaign")
 	public Campaign createCampaign(
 			Campaign prod) {
-//		return (Campaign) this.service.createProduct(prod);
-		return null;
+		return (Campaign) this.service.createProduct(prod);
 	}
 
+	@GET
+	@Operation(operationId = "readAllProducts")
 	public List<IndividualisedProductItem> readAllProducts() {
 		return (List)this.service.readAllProducts()
 				.stream()
@@ -58,16 +73,23 @@ public class ProductCRUDServiceOPIImpl {
 				.collect(Collectors.toList());
 	}
 
+	@PUT
+	@Operation(operationId = "updateProduct")
 	public IndividualisedProductItem updateProduct(
 			IndividualisedProductItem update) {
 		return (IndividualisedProductItem)this.service.updateProduct(update.getId(), update);
 	}
 
+	@DELETE
+	@Operation(operationId = "deleteProduct")
 	public boolean deleteProduct(long id) {
 		return this.service.deleteProduct(id);
 	}
 
-	public IndividualisedProductItem readProduct(long id) {
+	@GET
+	@Path("/{id}")
+	@Operation(operationId = "readProduct")
+	public IndividualisedProductItem readProduct(@PathParam("id") long id) {
 		IndividualisedProductItem item = (IndividualisedProductItem)this.service.readProduct(id);
 		return item;
 	}
--- org.dieschnittstelle.ess.opi.client/src/main/java/org/dieschnittstelle/ess/opi/client/junit/TestProductRESTServiceWithOpenAPI.java
+++ org.dieschnittstelle.ess.opi.client/src/main/java/org/dieschnittstelle/ess/opi/client/junit/TestProductRESTServiceWithOpenAPI.java
@@ -5,123 +5,122 @@ package org.dieschnittstelle.ess.opi.client.junit;
 //  generiert haben. Falls Ihre imports automatisch aktualisiert werden, dann entfernen Sie erst die Kommentare
 //  aus der Implementierung der Klasse und kommentieren Sie die imports erst danach ein.
 
-//import org.dieschnittstelle.ess.opi.client.entities.Campaign;
-//import org.dieschnittstelle.ess.opi.client.entities.IndividualisedProductItem;
-//import org.dieschnittstelle.ess.opi.client.entities.ProductBundle;
-//import org.dieschnittstelle.ess.opi.client.entities.ProductType;
-//import org.junit.Before;
-//import org.junit.FixMethodOrder;
-//import org.junit.Test;
-//import org.junit.runners.MethodSorters;
-//
-//import java.util.ArrayList;
-//import java.util.Arrays;
-//import java.util.List;
-//
-//import static org.junit.Assert.*;
-//
-//@FixMethodOrder(MethodSorters.NAME_ASCENDING)
-public class TestProductRESTServiceWithOpenAPI {
-//
-//    private ProductCRUDOpenAPIClient client;
-//
-//    private static IndividualisedProductItem PRODUCT_1 = new IndividualisedProductItem();
-//    private static IndividualisedProductItem PRODUCT_2 = new IndividualisedProductItem();
-//    private static Campaign CAMPAIGN = new Campaign();
-//    private static List<?> prodlistBefore = new ArrayList<>();
-//
-//    @Before
-//    public void prepareClient() throws Exception {
-//        // prepare the client
-//        client = new ProductCRUDOpenAPIClient();
-//    }
-//
-//    private void prepareProducts() {
-//        // prepare the products
-//        PRODUCT_1.setName("Schrippe");
-//        PRODUCT_1.setId(0L);
-//        PRODUCT_1.setPrice(-1);
-//        PRODUCT_1.setProductType(ProductType.ROLL);
-//        PRODUCT_1.setExpirationAfterStocked(720);
-//
-//        PRODUCT_2.setName("Kirschplunder");
-//        PRODUCT_2.setId(0L);
-//        PRODUCT_2.setPrice(-1);
-//        PRODUCT_2.setProductType(ProductType.PASTRY);
-//        PRODUCT_2.setExpirationAfterStocked(1080);
-//
-//        ProductBundle b1 = new ProductBundle();
-//        b1.setId(0L);
-//        b1.setUnits(10);
-//        b1.setProduct(PRODUCT_1);
-//        ProductBundle b2 = new ProductBundle();
-//        b2.setId(0L);
-//        b2.setUnits(3);
-//        b2.setProduct(PRODUCT_2);
-//        CAMPAIGN.setBundles(Arrays.asList(b1,b2));
-//        CAMPAIGN.setId(0L);
-//        CAMPAIGN.setPrice(-1);
-//        CAMPAIGN.setName("Testkampagne");
-//
-//    }
-//
-//    @Test
-//    public void a_readAll() {
-//        prepareProducts();
-//        // read all products
-//        assertNotNull("product list can be read", prodlistBefore = client.readAllProducts());
-//    }
-//
-//    @Test
-//    public void b_create() {
-//        // create two products
-//        client.createProduct(PRODUCT_1);
-//        client.createProduct(PRODUCT_2);
-//        assertEquals("product list is appended on create", 2, client.readAllProducts().size() - prodlistBefore.size());
-//    }
-//
-//    @Test
-//    public void c_read() {
-//        // read the products and check whether they are equivalent
-//        IndividualisedProductItem testProduct = client.readProduct(PRODUCT_1.getId());
-//
-//        assertNotNull("new product can be read", testProduct);
-//        assertEquals("new product name is correct", PRODUCT_1.getName(), testProduct.getName());
-//    }
-//
-//    @Test
-//    public void d_update() {
-//        // change the local name
-//        PRODUCT_1.setName(PRODUCT_1.getName() + " " + PRODUCT_1.getName());
-//        // update the product on the server-side
-//        client.updateProduct(PRODUCT_1);
-//
-//        // read out the product and compare the names
-//        IndividualisedProductItem testProduct = client.readProduct(PRODUCT_1.getId());
-//        assertEquals("product name is updated correctly", PRODUCT_1.getName(), testProduct.getName());
-//    }
-//
-//    @Test
-//    public void e_delete() {
-//        /* DELETE */
-//        assertTrue("product can be deleted", client.deleteProduct(PRODUCT_1.getId()));
-//        // given the dependencies from the generator, the following test results in a NoSuchMethodError as
-//        // int jakarta.ws.rs.core.Response.getLength() is not available - which is supposedly due to the
-//        // null response we receive here
-////		assertNull("deleted product does not exist anymore", client.readProduct(PRODUCT_1.getId()));
-//        assertEquals("product list is reduced on delete", prodlistBefore.size() + 1, client.readAllProducts().size());
-//    }
-//
-//    @Test
-//    public void f_createCampaign() {
-//        assertNotNull("original campaign contains bundles",CAMPAIGN.getBundles());
-//
-//        /* this is for internally testing that campaigns can be written and read via the web api - not part of the exercise */
-//        Campaign campaign = (Campaign)client.createCampaign(CAMPAIGN);
-//        assertNotEquals("campaign can be created",(long)0L,(long)campaign.getId());
-//        assertNotNull("campaign contains bundles",campaign.getBundles());
-//        assertEquals("campaign contains correct number of bundles",2,campaign.getBundles().size());
-//        assertNotNull("campaign bundles contain products",campaign.getBundles().get(0).getProduct());
-//    }
+import org.dieschnittstelle.ess.opi.client.entities.Campaign;
+import org.dieschnittstelle.ess.opi.client.entities.IndividualisedProductItem;
+import org.dieschnittstelle.ess.opi.client.entities.ProductBundle;
+import org.dieschnittstelle.ess.opi.client.entities.ProductType;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class TestProductRESTServiceWithOpenAPI {
+
+    private ProductCRUDOpenAPIClient client;
+
+    private static IndividualisedProductItem PRODUCT_1 = new IndividualisedProductItem();
+    private static IndividualisedProductItem PRODUCT_2 = new IndividualisedProductItem();
+    private static Campaign CAMPAIGN = new Campaign();
+    private static List<?> prodlistBefore = new ArrayList<>();
+
+    @Before
+    public void prepareClient() throws Exception {
+        // prepare the client
+        client = new ProductCRUDOpenAPIClient();
+    }
+
+    private void prepareProducts() {
+        // prepare the products
+        PRODUCT_1.setName("Schrippe");
+        PRODUCT_1.setId(0L);
+        PRODUCT_1.setPrice(-1);
+        PRODUCT_1.setProductType(ProductType.ROLL);
+        PRODUCT_1.setExpirationAfterStocked(720);
+
+        PRODUCT_2.setName("Kirschplunder");
+        PRODUCT_2.setId(0L);
+        PRODUCT_2.setPrice(-1);
+        PRODUCT_2.setProductType(ProductType.PASTRY);
+        PRODUCT_2.setExpirationAfterStocked(1080);
+
+        ProductBundle b1 = new ProductBundle();
+        b1.setId(0L);
+        b1.setUnits(10);
+        b1.setProduct(PRODUCT_1);
+        ProductBundle b2 = new ProductBundle();
+        b2.setId(0L);
+        b2.setUnits(3);
+        b2.setProduct(PRODUCT_2);
+        CAMPAIGN.setBundles(Arrays.asList(b1,b2));
+        CAMPAIGN.setId(0L);
+        CAMPAIGN.setPrice(-1);
+        CAMPAIGN.setName("Testkampagne");
+
+    }
+
+    @Test
+    public void a_readAll() {
+        prepareProducts();
+        // read all products
+        assertNotNull("product list can be read", prodlistBefore = client.readAllProducts());
+    }
+
+    @Test
+    public void b_create() {
+        // create two products
+        client.createProduct(PRODUCT_1);
+        client.createProduct(PRODUCT_2);
+        assertEquals("product list is appended on create", 2, client.readAllProducts().size() - prodlistBefore.size());
+    }
+
+    @Test
+    public void c_read() {
+        // read the products and check whether they are equivalent
+        IndividualisedProductItem testProduct = client.readProduct(PRODUCT_1.getId());
+
+        assertNotNull("new product can be read", testProduct);
+        assertEquals("new product name is correct", PRODUCT_1.getName(), testProduct.getName());
+    }
+
+    @Test
+    public void d_update() {
+        // change the local name
+        PRODUCT_1.setName(PRODUCT_1.getName() + " " + PRODUCT_1.getName());
+        // update the product on the server-side
+        client.updateProduct(PRODUCT_1);
+
+        // read out the product and compare the names
+        IndividualisedProductItem testProduct = client.readProduct(PRODUCT_1.getId());
+        assertEquals("product name is updated correctly", PRODUCT_1.getName(), testProduct.getName());
+    }
+
+    @Test
+    public void e_delete() {
+        /* DELETE */
+        assertTrue("product can be deleted", client.deleteProduct(PRODUCT_1.getId()));
+        // given the dependencies from the generator, the following test results in a NoSuchMethodError as
+        // int jakarta.ws.rs.core.Response.getLength() is not available - which is supposedly due to the
+        // null response we receive here
+//		assertNull("deleted product does not exist anymore", client.readProduct(PRODUCT_1.getId()));
+        assertEquals("product list is reduced on delete", prodlistBefore.size() + 1, client.readAllProducts().size());
+    }
+
+    @Test
+    public void f_createCampaign() {
+        assertNotNull("original campaign contains bundles",CAMPAIGN.getBundles());
+
+        /* this is for internally testing that campaigns can be written and read via the web api - not part of the exercise */
+        Campaign campaign = (Campaign)client.createCampaign(CAMPAIGN);
+        assertNotEquals("campaign can be created",(long)0L,(long)campaign.getId());
+        assertNotNull("campaign contains bundles",campaign.getBundles());
+        assertEquals("campaign contains correct number of bundles",2,campaign.getBundles().size());
+        assertNotNull("campaign bundles contain products",campaign.getBundles().get(0).getProduct());
+    }
 }
