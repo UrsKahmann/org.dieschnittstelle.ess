697996c574354084477c15fb52a07c0c67c8ae38
Urs Kahmann <urka3288@bht-berlin.de>
2023-06-06 09:08:55 +0200

    MIP+JPA Aufgabe 1

--- org.dieschnittstelle.ess.mip.components.erp.api/src/main/java/org/dieschnittstelle/ess/mip/components/erp/crud/api/ProductCRUD.java
+++ org.dieschnittstelle.ess.mip.components.erp.api/src/main/java/org/dieschnittstelle/ess/mip/components/erp/crud/api/ProductCRUD.java
@@ -2,27 +2,41 @@ package org.dieschnittstelle.ess.mip.components.erp.crud.api;
 
 import java.util.List;
 
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
 import org.dieschnittstelle.ess.entities.erp.AbstractProduct;
 import org.dieschnittstelle.ess.entities.erp.Campaign;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 /*
  * TODO MIP+JPA1/2/5:
  * this interface shall be implemented using an ApplicationScoped CDI bean with an EntityManager.
  * See TouchpointCRUDImpl for an example bean with a similar scope of functionality
  */
-
+@RegisterRestClient
+@Path("/product")
+@Produces({MediaType.APPLICATION_JSON})
+@Consumes({MediaType.APPLICATION_JSON})
 public interface ProductCRUD {
 
+	@POST
 	public AbstractProduct createProduct(AbstractProduct prod);
 
+	@GET
 	public List<AbstractProduct> readAllProducts();
 
+	@PUT
 	public AbstractProduct updateProduct(AbstractProduct update);
 
-	public AbstractProduct readProduct(long productID);
+	@GET
+	@Path("/{productId}")
+	public AbstractProduct readProduct(@PathParam("productId") long productID);
 
+	@DELETE
 	public boolean deleteProduct(long productID);
 
-	public List<Campaign> getCampaignsForProduct(long productID);
+	@GET
+	@Path("/{productId}/campaign")
+	public List<Campaign> getCampaignsForProduct(@PathParam("productId") long productID);
 
 }
